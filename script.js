04ede4fb30c6020da2dbfd2dad61850083330573
Iraneide Martins <iraneideml@gmail.com>
2023-11-18 16:47:45 -0300

    atv joy

---
+++ script.js
@@ -0,0 +1,18 @@
+let titulo = document.createElement("h1");
+
+titulo.id = "Título";
+titulo.innerText = "CASA DO SMARTPHONE";
+
+let body = document.querySelector("body");
+body.appendChild(titulo);
+
+let prod = document.createElement("div");
+titulo.id = "div";
+div.innerHTML=
+`
+<h1>Smartphone Xiaomi POCO X5 Pro 5G Dual SIM 8GB 256GB 6,67" FHD+ 108MP 5000mAh 67W Carregamento Cor Azul</h1>
+        <img src="./phone_.jpg" alt="smartphone xiaomi azul">
+        <h2>Preço: 1.490,00</h2>
+        <p>pix, cartão em até 12x</p>
+`;
+body.appendChild(prod);
